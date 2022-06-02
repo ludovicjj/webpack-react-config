a6407a75fdf0d247a309806e50f7d348b5f85e75
ludovicjj <jahanlud@gmail.com>
2022-06-02 17:23:07 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,45 @@
-const path = require('path');
-const ReactRefreshPlugin  = require('@pmmmwh/react-refresh-webpack-plugin');
+const path = require('path')
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 
 module.exports = {
     mode: isDevelopment ? 'development' : 'production',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/',
         clean: true,
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public'),
-        },
-        client: {
-            overlay: false,
-            logging: 'warn',
-        },
-        open: true,
-        port: 9000,
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
-                include: path.resolve(__dirname, 'src'),
                 exclude: /node_modules/,
                 use: 'babel-loader',
             },
             {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {},
-                    },
-                    "css-loader",
-                ],
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
-        ]
+        ],
+    },
+    resolve: {
+        extensions: ['.jsx', '.js'],
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true
     },
     plugins: [
-        isDevelopment && new ReactRefreshPlugin(),
-        new MiniCssExtractPlugin({
-            filename: isDevelopment ? "[name].css" : "[name].[contenthash].css",
-            chunkFilename: isDevelopment ? "[id].css" : "[id].[contenthash].css",
-        }),
         new HtmlWebpackPlugin({
             template: "./public/index.html"
         }),
-    ].filter(Boolean),
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
+        new MiniCssExtractPlugin(),
+        new ReactRefreshWebpackPlugin(),
+    ],
 }
